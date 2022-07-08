dd57ba79f399c9c8bf50a65f20d7e27e764b3fc2
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-08 13:16:44 -0400

    feat: created deposit function

---
+++ ethereum/RoyaltyBaseContract.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract RoyaltyBaseContract {
+    mapping(address => uint) public balances;
+
+    constructor() {}
+
+    function deposit() payable public {
+        balances[msg.sender] += msg.value;
+    }
+}
